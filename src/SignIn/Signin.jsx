e4e9e6735a2d48e1d509ef3c1c30fc9de35feeee
Omar Ahmed El-Hanafy <106983382+OmarAhmedEl-hanafy@users.noreply.github.com>
2024-05-13 02:15:11 +0300

    Update Signin.jsx

--- src/SignIn/Signin.jsx
+++ src/SignIn/Signin.jsx
@@ -19,7 +19,7 @@ function Signin() {
 
   const handleSignIn = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:8000/Login/User/", {
+      const response = await fetch("http://13.60.94.144:8000/Login/User/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
